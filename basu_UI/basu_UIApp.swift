6110f0c7442f1501a00e7a2317e90fd7a10f52cb
むぎ <95331428+mugiaoo@users.noreply.github.com>
2023-09-27 11:55:43 +0900

    Add files via upload

---
+++ basu_UI/basu_UIApp.swift
@@ -0,0 +1,17 @@
+//
+//  basu_UIApp.swift
+//  basu_UI
+//
+//  Created by Mugi on 2023/09/27.
+//
+
+import SwiftUI
+
+@main
+struct basu_UIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
