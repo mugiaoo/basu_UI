f1cc61d5b84602564662a1c5598a8ca2daa33625
mugiaoo <b1020191@fun.ac.jp>
2023-09-27 14:51:49 +0900

    こんふり解消

--- basu_UI/ContentView.swift
+++ basu_UI/ContentView.swift
@@ -13,11 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-<<<<<<< HEAD
             Text("こんにちは")
-=======
-            Text("Hello, world!")
->>>>>>> 6110f0c7442f1501a00e7a2317e90fd7a10f52cb
         }
         .padding()
     }
